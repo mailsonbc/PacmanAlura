d0aa32b982a3d4c50e1ef49ac3ba12984f2a5c98
Mailson Barreto <mailsonbc@gmail.com>
2021-10-21 22:22:39 -0300

    Criadas a funções principais do jogo / Criado o loop principal e feito testes de desenho com o pygame

--- main.py
+++ main.py
@@ -1,14 +1,50 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# Santos, 21 de outubro de 2021
+# Iniciando o curso de criação de jogos da Alura
+# Criando um jogo similar ao Pacman
+##################################################################################################################
 import pygame as pg
 
+#Constantes
+AMARELO = (255, 255, 0)
+PRETO = (0, 0, 0)
 
+
+#################################################################################################################
+#Função onde o jogo ocorre
 def pacman():
     pg.init()
+    #############################################################################################################
+    tela = pg.display.set_mode((640, 480), 0)
+    x = 50
+    vel_x = 0.1
+    y = 50
+    vel_y = 0.1
+    jogando = True
+    #############################################################################################################
+
+    #loop principal do jogo
+    while jogando:
+
+        x = x + vel_x
+        y = y + vel_y
+        if (x + 30) > 640:
+            vel_x = -0.1
+        elif (x - 30) < 0:
+            vel_x = 0.1
+        if (y + 30) > 480:
+            vel_y = -0.1
+        elif (y - 30) < 0:
+            vel_y = 0.1
+        tela.fill((PRETO))
+        pg.draw.circle(tela, AMARELO, (int(x), int(y)), 25, 0)
+        pg.display.update()
+
+        for e in pg.event.get():
+            if e.type == pg.QUIT:
+                exit()
 
 
+##################################################################################################################
+#Função principal para iniciar o programa
 if __name__ == '__main__':
     pacman()
