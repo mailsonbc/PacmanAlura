bfe2029831344f5835f7bbc505822f775309324c
Mailson Barreto <mailsonbc@gmail.com>
2021-10-26 21:23:06 -0300

    Desenhado o labirinto

--- pacman_2.py
+++ pacman_2.py
@@ -6,40 +6,84 @@ import pygame as pg
 
 # Constantes
 AMARELO = (255, 255, 0)
+AZUL = (0, 0, 255)
 PRETO = (0, 0, 0)
 FPS = 60
 TELA_X = 800
 TELA_Y = 600
+VELOCIDADE = 1
 
 
 #################################################################################################################
 # Classes
+class Cenario:
+    def __init__(self, tamanho):
+        self.tamanho = tamanho
+        self.matriz = [
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 0, 0, 0, 0, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 2, 2, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+        ]
+
+    def pintar_linha(self, tela, numero_linha, linha):
+        for numero_coluna, coluna in enumerate(linha):
+            x = numero_coluna * self.tamanho
+            y = numero_linha * self.tamanho
+            metade = self.tamanho // 2
+            cor = PRETO
+            if coluna == 2:
+                cor = AZUL
+            pg.draw.rect(tela, cor, (x, y, self.tamanho, self.tamanho), 0)
+            if coluna == 1:
+                pg.draw.circle(tela, AMARELO, (x + metade, y + metade), self.tamanho // 10, 0)
+
+    def pintar(self, tela):
+        for numero_linha, linha in enumerate(self.matriz):
+            self.pintar_linha(tela, numero_linha, linha)
+
+
 class Pacman:
-    def __init__(self):
+    def __init__(self, tamanho):
         self.linha = 1
         self.coluna = 1
         self.centro_x = 400
         self.centro_y = 300
-        self.tamanho = TELA_X // 30
-        self.vel_x = 1
-        self.vel_y = 1
+        self.tamanho = tamanho
+        self.vel_x = 0
+        self.vel_y = 0
         self.raio = self.tamanho // 2
 
-    def calcular_regras (self):
+    def calcular_regras(self):
         self.coluna = self.coluna + self.vel_x
         self.linha = self.linha + self.vel_y
         self.centro_x = int(self.coluna * self.tamanho + self.raio)
         self.centro_y = int(self.linha * self.tamanho + self.raio)
-
-        if self.centro_x + self.raio > TELA_X:
-            self.vel_x = -1
-        if self.centro_x - self.raio < 0:
-            self.vel_x = 1
-        if self.centro_y + self.raio > TELA_Y:
-            self.vel_y = -1
-        if self.centro_y - self.raio < 0:
-            self.vel_y = 1
-
 
     # Método para desenhar o pacman
     def pintar(self, tela):
@@ -59,11 +103,44 @@ class Pacman:
         olho_raio = int(self.raio / 10)
         pg.draw.circle(tela, PRETO, (olho_x, olho_y), olho_raio, 0)
 
+    def processar_eventos(self, eventos):
+        for e in eventos:
+            if e.type == pg.KEYDOWN:
+                if e.key == pg.K_RIGHT:
+                    self.vel_x = VELOCIDADE
+                elif e.key == pg.K_LEFT:
+                    self.vel_x = -VELOCIDADE
+                elif e.key == pg.K_UP:
+                    self.vel_y = -VELOCIDADE
+                elif e.key == pg.K_DOWN:
+                    self.vel_y = VELOCIDADE
+            elif e.type == pg.KEYUP:
+                if e.key == pg.K_RIGHT:
+                    self.vel_x = 0
+                elif e.key == pg.K_LEFT:
+                    self.vel_x = 0
+                elif e.key == pg.K_UP:
+                    self.vel_y = 0
+                elif e.key == pg.K_DOWN:
+                    self.vel_y = 0
+
+    # def processar_eventos_mouse(self, eventos):
+    #    delay = 40
+    #    for e in eventos:
+    #        if e.type == pg.MOUSEMOTION:
+    #            mouse_x, mouse_y = e.pos
+    #            self.coluna = (mouse_x - self.centro_x) / delay
+    #            self.linha = (mouse_y - self.centro_y) / delay
+
+
 #################################################################################################################
 # Função onde o jogo ocorre
 def pacman():
     # criando o objeto pacman
-    pacman = Pacman()
+    size = TELA_Y // 30
+
+    pacman = Pacman(size)
+    cenario = Cenario(size)
 
     pg.init()
     tela = pg.display.set_mode((TELA_X, TELA_Y), 0)
@@ -72,16 +149,19 @@ def pacman():
     # loop principal do jogo
     while jogando:
 
-
         tela.fill(PRETO)
+        cenario.pintar(tela)
         pacman.pintar(tela)
         pacman.calcular_regras()
         pg.display.update()
         pg.time.delay(100)
 
-        for e in pg.event.get():
+        # Captura de eventos
+        eventos = pg.event.get()
+        for e in eventos:
             if e.type == pg.QUIT:
                 exit()
+        pacman.processar_eventos(eventos)
 
 
 ##################################################################################################################
